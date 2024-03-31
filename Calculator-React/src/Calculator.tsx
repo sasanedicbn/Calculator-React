e9d4a9f8bb82428233193087db5ab6dba75e3328
Sasa Nedic <svetasmak0@gmail.com>
2024-04-01 00:56:28 +0200

    Create OutputNumbers functions

--- Calculator-React/src/Calculator.tsx
+++ Calculator-React/src/Calculator.tsx
@@ -1,41 +1,50 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 
 const Calculator = () => {
-  const [firstOutput, setFirstOutput] = useState(0)
-  const [secondOutput, setSecondOutput] = useState('')
-  const [operation, setOperation] = useState('')
+    const [firstOutput, setFirstOutput] = useState("0");
+    const [secondOutput, setSecondOutput] = useState('');
+    const [operation, setOperation] = useState('');
 
-  return (
-    <div className="calculator-container"> 
-      <div className="calculator">
-        <div className="output-secondary">{secondOutput}</div>
-        <div className="output-first">{firstOutput}</div>
-      </div>
-      <div className="buttons">
-        <button className="btn btn-clear">CLEAR</button>
-        <button className="btn btn-delete">DELETE</button>
-        <button className="btn btn-number">7</button>
-        <button className="btn btn-number">8</button>
-        <button className="btn btn-number">9</button>
-        <button className="btn btn-operation">/</button>
+    function OutputNumbers(event) {
+        const number = event.target.textContent;
+        if (operation !== '') {
+            setSecondOutput((prevOutput) => prevOutput + number);
+        } else {
+           
+        }
+    }
 
-        <button className="btn btn-number">4</button>
-        <button className="btn btn-number">5</button>
-        <button className="btn btn-number">6</button>
-        <button className="btn btn-operation">*</button>
+    return (
+        <div className="container">
+            <div className="calculator">
+                <div className="output-secondary">{secondOutput}</div>
+                <div className="output-first">{firstOutput}</div>
+            </div>
+            <div className="buttons">
+                <button className="btn btn-clear">CLEAR</button>
+                <button className="btn btn-delete">DELETE</button>
+                <button className="btn btn-number" onClick={OutputNumbers}>7</button>
+                <button className="btn btn-number" onClick={OutputNumbers}>8</button>
+                <button className="btn btn-number" onClick={OutputNumbers}>9</button>
+                <button className="btn btn-operation">/</button>
 
-        <button className="btn btn-number">1</button>
-        <button className="btn btn-number">2</button>
-        <button className="btn btn-number">3</button>
-        <button className="btn btn-operation">-</button>
+                <button className="btn btn-number" onClick={OutputNumbers}>4</button>
+                <button className="btn btn-number" onClick={OutputNumbers}>5</button>
+                <button className="btn btn-number" onClick={OutputNumbers}>6</button>
+                <button className="btn btn-operation">*</button>
 
-        <button className="btn btn-dot">.</button>
-        <button className="btn btn-number">0</button>
-        <button className="btn btn-operation-equal">=</button>
-        <button className="btn btn-operation">+</button>
-      </div>
-    </div>
-  );
+                <button className="btn btn-number" onClick={OutputNumbers}>1</button>
+                <button className="btn btn-number" onClick={OutputNumbers}>2</button>
+                <button className="btn btn-number" onClick={OutputNumbers}>3</button>
+                <button className="btn btn-operation">-</button>
+
+                <button className="btn btn-dot">.</button>
+                <button className="btn btn-number" onClick={OutputNumbers}>0</button>
+                <button className="btn btn-operation-equal">=</button>
+                <button className="btn btn-operation">+</button>
+            </div>
+        </div>
+    );
 };
 
 export default Calculator;
