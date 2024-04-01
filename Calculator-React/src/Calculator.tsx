e9a5768ea1e458b804b8e7674c73887a8d0f67e5
Sasa Nedic <svetasmak0@gmail.com>
2024-04-01 20:25:31 +0200

    Add more code

--- Calculator-React/src/Calculator.tsx
+++ Calculator-React/src/Calculator.tsx
@@ -5,28 +5,33 @@ const Calculator = () => {
     const [secondOutput, setSecondOutput] = useState('');
     const [operation, setOperation] = useState('');
 
-    function OutputNumbers(event) {
-        const number = event.target.textContent;
-        if (operation !== '') {
-            // setSecondOutput((prevOutput) => prevOutput + number);
-        } else {
-            if (firstOutput === '0') {
-                setFirstOutput(number);
-            } else {
-                setFirstOutput((prevOutput) => prevOutput + number);
-            }
+    function resetCalculator() {
+        setFirstOutput("0");
+        setSecondOutput("");
+        setOperation("");
+    }
+
+    function inputNumber(number) {
+        if (!operation || (operation && !secondOutput)) {
+            setFirstOutput((prevOutput) => prevOutput === "0" ? number : prevOutput + number);
+        } else if (operation && secondOutput) {
+            setFirstOutput((prevOutput) => prevOutput + number);
         }
     }
 
-    function OutputOperation(event) {
-        const selectedOperation = event.target.textContent;
-        if (!firstOutput) {
-            return; 
-        } else {
-            setOperation(selectedOperation);
-            setFirstOutput((prevOutput) => prevOutput + selectedOperation); 
+    function setOperationFunc(op) {
+        if (operation !== "") {
+            logicOperation();
         }
+
+        setOperation(op);
+        setSecondOutput(`${firstOutput}${op}`);
+        setFirstOutput("");
     }
+
+  
+
+   
 
     return (
         <div className="container">
@@ -35,30 +40,43 @@ const Calculator = () => {
                 <div className="output-first">{firstOutput}</div>
             </div>
             <div className="buttons">
-                <button className="btn btn-clear">CLEAR</button>
-                <button className="btn btn-delete">DELETE</button>
-                <button className="btn btn-number" onClick={OutputNumbers}>7</button>
-                <button className="btn btn-number" onClick={OutputNumbers}>8</button>
-                <button className="btn btn-number" onClick={OutputNumbers}>9</button>
-                <button className="btn btn-operation" onClick={OutputOperation}>/</button>
+                <button className="btn btn-clear" onClick={resetCalculator}>CLEAR</button>
+                <button className="btn btn-delete" onClick={deleteOneElement}>DELETE</button>
+                <button className="btn btn-number" onClick={() => inputNumber('7')}>7</button>
+                <button className="btn btn-number" onClick={() => inputNumber('8')}>8</button>
+                <button className="btn btn-number" onClick={() => inputNumber('9')}>9</button>
+                <button className="btn btn-operation" onClick={() => setOperationFunc('/')}>/</button>
 
-                <button className="btn btn-number" onClick={OutputNumbers}>4</button>
-                <button className="btn btn-number" onClick={OutputNumbers}>5</button>
-                <button className="btn btn-number" onClick={OutputNumbers}>6</button>
-                <button className="btn btn-operation" onClick={OutputOperation}>*</button>
+                <button className="btn btn-number" onClick={() => inputNumber('4')}>4</button>
+                <button className="btn btn-number" onClick={() => inputNumber('5')}>5</button>
+                <button className="btn btn-number" onClick={() => inputNumber('6')}>6</button>
+                <button className="btn btn-operation" onClick={() => setOperationFunc('*')}>*</button>
 
-                <button className="btn btn-number" onClick={OutputNumbers}>1</button>
-                <button className="btn btn-number" onClick={OutputNumbers}>2</button>
-                <button className="btn btn-number" onClick={OutputNumbers}>3</button>
-                <button className="btn btn-operation" onClick={OutputOperation}>-</button>
+                <button className="btn btn-number" onClick={() => inputNumber('1')}>1</button>
+                <button className="btn btn-number" onClick={() => inputNumber('2')}>2</button>
+                <button className="btn btn-number" onClick={() => inputNumber('3')}>3</button>
+                <button className="btn btn-operation" onClick={() => setOperationFunc('-')}>-</button>
 
-                <button className="btn btn-dot">.</button>
-                <button className="btn btn-number" onClick={OutputNumbers}>0</button>
-                <button className="btn btn-operation-equal">=</button>
-                <button className="btn btn-operation" onClick={OutputOperation}>+</button>
+                <button className="btn btn-dot" onClick={inputDecimal}>.</button>
+                <button className="btn btn-number" onClick={() => inputNumber('0')}>0</button>
+                <button className="btn btn-operation-equal" onClick={logicOperation}>=</button>
+                <button className="btn btn-operation" onClick={() => setOperationFunc('+')}>+</button>
             </div>
         </div>
     );
 };
 
 export default Calculator;
+
+
+{/* <div className='container'>
+    <img src="link slike"/>
+    <h3>Jessica Randal</h3>
+    <p>London, United Kingdom</p>
+    <p>Front-end developer ...</p>
+    <p>GitHub</p>
+    <p>Frontend Mentor</p>
+    <p>LinkedIn</p>
+    <p>Twitter</p>
+    <p>Instagram</p>
+</div> */}
